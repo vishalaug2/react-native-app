68792e4dbd8704e2c62ab56c0af57e8f4c3f1e30
Vishal Agarwal <vishal.aug2@gmail.com>
2020-06-06 16:50:04 +0530

    Added validation

--- screens/login/Login.js
+++ screens/login/Login.js
@@ -1,37 +1,76 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { Container, Header, Content, Text, H1, H2, H3, Form, Item, Input, Label, Button, Picker, Icon } from 'native-base';
+import { validateAll } from 'indicative/validator'
 
-export default function Login({ navigation }) {
-   return (
+export default class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      errors: {}
+    }
+  }
+
+  checkLogin() {
+    const rules = {
+      username  : 'required',
+      password  : 'required'
+    };
+
+    const messages = {
+      'username.required' : 'Username is required',
+      'password.required'    : 'Password is required'
+    }
+
+    validateAll(this.state, rules, messages)
+    .then(() => {
+      this.setState({errors: {}});
+      this.props.navigation.navigate('DoctorDrawer');
+    })
+    .catch((errors) => {
+      let formattedErrors = {};
+      errors.forEach(error => {
+        formattedErrors[error.field] = error.message;
+      });
+      this.setState({errors: formattedErrors});
+    });
+  }
+
+  render() {
+    return (
       <Container>
-        <Content style={styles.content}>        
-        <H2>
-          Login
+        <Content style={styles.content}>
+          <H2>
+            Login
         </H2>
-        <Text style={styles.headingdesc}>Please sign in to continue</Text>
-        <Form>
-            <Item floatingLabel>
+          <Text style={styles.headingdesc}>Please sign in to continue</Text>
+          <Form>
+            <Item stackedLabel error={(this.state.errors['username'] ? true : false)}>
               <Label>Username</Label>
-              <Input />
+              <Input name="username" onChangeText={(username) => this.setState({username})} value={this.state.username} />
+              {this.state.errors['username'] && <Icon name='close-circle' />}
             </Item>
-            <Item floatingLabel last>
+            <Item stackedLabel error={(this.state.errors['password'] ? true : false)}>
               <Label>Password</Label>
-              <Input secureTextEntry={true} />
+              <Input secureTextEntry={true} name="password"  onChangeText={(password) => this.setState({password})} value={this.state.password} />
+              {this.state.errors['password'] && <Icon name='close-circle' />}
             </Item>
-            <Button onPress={() => {navigation.navigate('DoctorDrawer')}}  iconLeft primary style={styles.button}>
+            <Button onPress={this.checkLogin.bind(this)} iconLeft primary style={styles.button}>
               <Icon type="FontAwesome" name='arrow-right' />
               <Text>Login</Text>
             </Button>
-            <Button onPress={() => {navigation.navigate('Signup')}} iconLeft success style={styles.button}>
+            <Button onPress={() => { this.props.navigation.navigate('Signup') }} iconLeft success style={styles.button}>
               <Icon type="FontAwesome" name='user' />
               <Text>Sign up new Patient</Text>
             </Button>
           </Form>
-      </Content>
+        </Content>
       </Container>
     );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -44,6 +83,6 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
   headingdesc: {
-      color: '#C0C0C0'
+    color: '#C0C0C0'
   }
 });
